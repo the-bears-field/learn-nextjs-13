86cb2b9d85ca366c6078c15b70b070caa7d121db
the-bears-field <thebearsfield+github@gmail.com>
2023-09-26 14:58:42 +0900

    feat: Learn Next.jsの"Metadata"を参考に編集
    /posts/first-postのtitleを変更
    学習サイトの内容ではうまくできなかったため、最新の公式ドキュメントのMetadataのページを参照し対応した。
    
    参考
    https://nextjs.org/learn/basics/assets-metadata-css/metadata
    https://nextjs.org/docs/app/building-your-application/optimizing/metadata
    https://zenn.dev/yamadadayo123/articles/6cb4f586de0183#seo%E3%82%BF%E3%82%B0%E3%82%92%E7%B0%A1%E5%8D%98%E3%81%AB%E8%A8%AD%E5%AE%9A%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%88%E3%81%86%E3%81%AB%E3%81%AA%E3%81%A3%E3%81%9F

--- src/app/posts/first-post/page.tsx
+++ src/app/posts/first-post/page.tsx
@@ -1,5 +1,10 @@
+import type { Metadata } from "next";
 import Link from "next/link";
 import { ReactNode } from "react";
+
+export const metadata: Metadata = {
+  title: 'First Post',
+}
 
 export default function FirstPost(): ReactNode {
   return (
