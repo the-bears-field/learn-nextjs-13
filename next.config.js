57317f0836863b89c48c7124e4f226d52ed0c2bb
the-bears-field <thebearsfield+github@gmail.com>
2023-12-22 16:45:01 +0900

    feat: ArticleListコンポーネントのJSXを変更
    Next.jsからImageとLinkコンポーネントをインポート。
    教材の内容に従い、JSXを記述。
    
    外部画像の許可設定については、ドキュメントを参考に、現状に沿った設定方法でnext.config.jsに記述。
    
    参考:
    19. ブログ記事投稿一覧をHTMLとTailwindCSSで作ってみよう：その１
    https://www.udemy.com/course/nextjs13_learning_with_microblog/learn/lecture/38891034
    https://nextjs.org/docs/app/api-reference/components/link
    https://nextjs.org/docs/app/api-reference/components/image

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+    ],
+  },
+
   // hot reload
   webpack: (config) => {
     config.watchOptions = {
--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ArticleList = () => {
-  return <div>ArticleList</div>;
+  return (
+    <div>
+      <article>
+        <Link href="/">
+          <Image
+            src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+            alt=""
+            width={1280}
+            height={300}
+          />
+        </Link>
+        <div>
+          <Link href="#">Technology</Link>
+          <Link href="#">Next.jsの勉強中</Link>
+          <p>By shincode, published on 2023/07/05</p>
+          <Link href="#">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quo
+            eos commodi aut repudiandae autem accusamus a voluptas natus
+            possimus tenetur tempore, et, illum exercitationem suscipit libero
+            esse quasi illo?
+          </Link>
+          <Link href="#">続きを読む</Link>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default ArticleList;
